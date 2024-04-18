d3b051e24ad85d8002faeb27667e103378d9ebfb
shivaprasadnashte <nashted.ug21.ee@nitp.ac.in>
2024-04-19 01:52:50 +0530

    Add path and fileURLToPath imports

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./utills/db.js";
 import userRouter from "./routes/user.routes.js";
+import path from "path"
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 dotenv.config();
 const app = express();
@@ -10,10 +13,17 @@ app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 connectDB();
 
+app.get("/",(req,res)=>{
+
+  res.sendFile(path.join(__dirname,"/public/index.html"))
+})
 app.use("/api/users",userRouter);
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT} `);
